d6c2ea4bbdfbf5bed96ab87d0a65d0abb0ec40f9
hazzang <54571875+hazzang@users.noreply.github.com>
2020-11-19 21:02:16 +0900

    JS class
    class 스크립트

--- HazzangCoding/javascript/class.js
+++ HazzangCoding/javascript/class.js
@@ -54,3 +54,75 @@ class User {
 const user1 = new User('Steve', 'job', -1);
 console.log(user1.age);
 
+//3. Fields (public, private)
+//Too soon!
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/ 
+class Experiment {
+    publicField = 2;
+    #privateField = 0;
+}
+
+const experiment = new Experiment();
+console.log(experiment.publicField);
+console.log(experiment.privateField);
+
+//4. Static properties and methods 
+//Too soon!
+class Article {
+    static publisher = 'Dream Coding';
+    constructor(articleNumber) {
+        this.articleNumber = articleNumber;
+    }
+
+    static printPublicsher() {
+        console.log(Article.publisher);
+    }
+}
+
+const article1 = new article1();
+const article2 = new article2();
+console.log(Article.publisher);
+Article.printPublicsher();
+
+
+//5. Inheritance
+//a way for one class to extend another class.
+class Shape {
+    constructor(width, height, color) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+    
+    draw() {
+        console.log('drawing ${this.color} color of')
+    }
+
+    getArea() {
+        return width * this.height;
+    }
+}
+
+class Rectangle extends Shape {}
+class Triangle extends Shape {
+
+    draw() {
+        super.draw();
+        console.log('^^');
+    }
+
+    getArea() {
+        return (this.width * this.height) / 2;
+    }
+}
+const Rectangle = new Rectangle(20, 20, 'blue');
+Rectangle.draw();
+
+//6. Class checking: instanceOf
+console.log(rectangle instanceof Rectangle);
+console.log(Triangle instanceof Rectangle);
+console.log(Triangle instanceof Triangle);
+console.log(Triangle instanceof shape);
+console.log(Triangle instanceof object);
+console.log(triangle.toString());
+
