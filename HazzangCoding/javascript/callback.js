bee8c7bc793d97a9ddcc3498b480d318ce1431f2
hazzang <54571875+hazzang@users.noreply.github.com>
2020-11-23 17:29:29 +0900

    JS callback
    js

--- HazzangCoding/javascript/callback.js
+++ HazzangCoding/javascript/callback.js
@@ -7,17 +7,32 @@ console.log('1');
 setTimeout(() => console.log('2'), 1000);
 console.log('3');
 
+
 //Synchronous callback
-function printImmediately(print){
+function printImmediately(print) {
     print();
 }
-printImmediately(() => console.log('hellow'));
-
+printImmediately(() => console.log('하짱~~'));
 
 //Asynchronous callback
 function printWithDelay(print, timeout) {
     setTimeout(print, timeout);
 }
-printWithDelay(() => console.log('async callbak'), 2000);
+printWithDelay(() => console.log('hi~~'), 2000);
+
 
 //Callback Hell example
+class UserStorage {
+    loginUser(id, password, onSuccess, onError) {
+
+    }
+
+    getRoles(user)
+}
+
+
+
+
+
+
+
